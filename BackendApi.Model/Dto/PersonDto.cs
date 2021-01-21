eabdb25089f57180a2558236eacf9b7f74043b1c
Deniz Turkmen <turkmen_deniz@hotmail.com>
2021-01-21 19:38:20 +0300

    Model Katmanı

---
+++ BackendApi.Model/Dto/PersonDto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BackendApi.Model.Dto
+{
+    public class PersonDto
+    {
+
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(50)]
+        public string Adi { get; set; }
+
+        [Required]
+        [MaxLength(length:50,ErrorMessage = "Zorunlu Alandır")]
+        public string Soyadi { get; set; }
+
+        [Required]
+        [MaxLength(length: 11, ErrorMessage = "Zorunlu Alandır")]
+        //[RegularExpression(pattern:"\\D")]
+        public int Telefonu { get; set; }
+
+
+        [Required]
+        [MaxLength(length: 11, ErrorMessage = "Zorunlu Alandır")]
+        public string Email { get; set; }
+
+        public bool Silindi_mi { get; set; }
+        
+        public bool Aktif { get; set; }
+    }
+}
---
+++ BackendApi.Model/Entity/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BackendApi.Model.Entity
+{
+    [Table(name:"Person")]
+    public class Person
+    {
+        [Column(name:"Id")]
+        public int Id { get; set; }
+        
+        [Column(name: "Adi")]
+        public string Adi { get; set; }
+        
+        [Column(name: "Soyadi")]
+        public string Soyadi { get; set; }
+        
+        [Column(name: "Telefonu")]
+        public int Telefonu { get; set; }
+        
+        [Column(name: "Email")]
+        public string Email { get; set; }
+        
+        [Column(name: "Silindi_mi")]
+        public bool Silindi_mi { get; set; }
+
+        [Column(name: "Aktif")]
+        public bool Aktif { get; set; }
+    }
+}
