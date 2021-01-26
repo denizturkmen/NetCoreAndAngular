2eaba72c5c329fa01227fc9b5e64c2a1e8730567
Deniz Turkmen <turkmen_deniz@hotmail.com>
2021-01-26 20:52:39 +0300

    Business layer active

---
+++ BackendApi.Business/Abstract/IPersonService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BackendApi.Model.Entity;
+
+namespace BackendApi.Business.Abstract
+{
+    public interface IPersonService
+    {
+        public List<Person> PersonList();
+        public Person PersonGetId(int id);
+        public void Create(Person saveEntity);
+        public void Update(Person updateEntity);
+        public void Delete(Person deleteEntity);
+    }
+}
---
+++ BackendApi.Business/Concrete/PersonManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BackendApi.Business.Abstract;
+using BackendApi.DataAccessLayer.Abstract;
+using BackendApi.Model.Entity;
+
+namespace BackendApi.Business.Concrete
+{
+    public class PersonManager : IPersonService
+    {
+        private IPersonDal _personDal;
+
+        public PersonManager(IPersonDal personDal)
+        {
+            _personDal = personDal;
+        }
+        public List<Person> PersonList()
+        {
+            return _personDal.GetAll();
+        }
+
+        public Person PersonGetId(int id)
+        {
+            return _personDal.GetById(id);
+        }
+
+        public void Create(Person saveEntity)
+        {
+            _personDal.Create(saveEntity);
+        }
+
+        public void Update(Person updateEntity)
+        {
+           _personDal.Update(updateEntity);
+        }
+
+        public void Delete(Person deleteEntity)
+        {
+            _personDal.Delete(deleteEntity);
+        }
+    }
+}
